2457be5bf63bfdd1245f13e1d1d757285937b398
UltraBlackLinux <accoundshit@protonmail.com>
2021-06-15 16:19:28 +0200

    random stuff again

--- src/main/java/me/ultrablacklinux/fixmygg/FixMyGG.java
+++ src/main/java/me/ultrablacklinux/fixmygg/FixMyGG.java
@@ -3,21 +3,13 @@ package me.ultrablacklinux.fixmygg;
 import me.ultrablacklinux.fixmygg.command.FixMyGGCommand;
 import me.ultrablacklinux.fixmygg.config.Config;
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class FixMyGG implements ModInitializer {
-    private static final Logger LOGGER = LogManager.getLogger();
     public static boolean skipCheck = false;
 
     @Override
     public void onInitialize() {
         Config.init();
         FixMyGGCommand.registerCommands();
-        LOGGER.info("[FixMyGG] Started");
-    }
-
-    public static Logger getLogger() {
-        return LOGGER;
     }
 }
